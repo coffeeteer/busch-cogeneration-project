e95f7bafcd06d8c7c2ce189fbc4a15f304404991
Coffeeteer <tgrieshiemer@gmail.com>
2017-04-30 20:49:41 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express        = require('express');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var http           = require('http');
+var path           = require('path');
+
+var app            = express();
+
+console.log('process.env.PORT', process.env.PORT);
+var port = process.env.PORT || 3010;
+ 
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function(req, res) {
+    res.sendFile( path.join(__dirname + '/public/views/index.html'));
+});
+
+app.listen(port, function() {
+    console.log('connected to port ', port);
+});
