1d9afe5d068899ab7d7d6e46d12a44d65385d9d4
Coffeeteer <tgrieshiemer@gmail.com>
2017-05-01 15:34:32 -0400

    made some changes to the design, plus added lightbox

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 	console.log('script loaded');
+
+	lightbox.option({
+      resizeDuration: 200,
+      wrapAround: false,
+      fitImagesInViewport: true,
+      maxWidth: 100%,
+    })
 });
